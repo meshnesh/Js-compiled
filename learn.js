5f049481c0c62ee5dcd5c4b04f19f65f5f4fa912
meshnesh <tonnie.nesh@gmail.com>
2016-05-01 23:20:10 +0300

    #2

---
+++ learn.js
@@ -0,0 +1,9 @@
+// document.getElementById("demo").innerHTML = "Hello Javascript";
+
+// alert("am alert box")
+
+
+var me = prompt("please enter your name");
+if (me != null){
+    document.getElementById("demo").innerHTML = "Hello" + me + "! how are you today?";
+}
